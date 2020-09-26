3d6e4d75686715e485c3d5668f395ff53e8eed88
TieuKhang <52084792+TieuKhang@users.noreply.github.com>
2020-09-26 07:24:29 -0400

    Update

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const mongoose = require("mongoose");
 // package that allows parsing info sent from the post request
 const bodyParser = require("body-parser");
 
+// set up authentication
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 // initialize app
 const app = express();
 
@@ -18,6 +23,17 @@ app.use(express.static("public"));
 
 // connect to ejs
 app.set('view engine', 'ejs');
+
+//initialize session
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // application on home route, we'll render the list page
 app.route("/")
